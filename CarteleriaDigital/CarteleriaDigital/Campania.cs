85d3c08be634b39a6693a722a45dfaf6372e2c89
Daniel Adrián Grinman <dan.gr04@live.com>
2017-01-17 08:54:42 -0300

    Hola mundo 2

---
+++ CarteleriaDigital/CarteleriaDigital/Campania.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarteleriaDigital
+{
+    class Campania
+    {
+        private Boolean iActivo;
+        private String iNombre;
+        private List<Imagen> iListaImagenes;
+
+        public Campania(Boolean pActivo, String pNombre, List<Imagen> pListaImagenes)
+        {
+            this.iActivo = pActivo;
+            this.iNombre = pNombre;
+            this.iListaImagenes = pListaImagenes;
+        }
+
+        public Boolean Activo
+        {
+            get { return this.iActivo; }
+            set { this.iActivo = value; }
+        }
+
+        public String Nombre
+        {
+            get { return this.Nombre; }
+            set { this.iNombre = value; }
+        }
+
+        public List<Imagen> ListaImagenes
+        {
+            get { return this.iListaImagenes; }
+            set { this.iListaImagenes = value; }
+        }
+    }
+}
---
+++ CarteleriaDigital/CarteleriaDigital/Imagen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarteleriaDigital
+{
+    class Imagen
+    {
+        private String iRutaImagen;
+        private short iDuracion;
+
+        public Imagen(String pRutaImagen, short pDuracion)
+        {
+            this.iDuracion = pDuracion;
+            this.iRutaImagen = pRutaImagen;
+        }
+
+        public short Duracion
+        {
+            set { iDuracion = value; }
+            get { return this.iDuracion; }
+        }
+
+        public String RutaImagen
+        {
+            set { iRutaImagen = value; }
+            get { return this.iRutaImagen; }
+        }
+    }
+}
