24a0740e9b901855a917227d0399a633f03a1034
Daniel Adrián Grinman <dan.gr04@live.com>
2017-01-17 11:31:23 -0300

    Intento numero 400

---
+++ CarteleriaDigital/CarteleriaDigital/Pantallas/Campaña.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarteleriaDigital
+{
+    public partial class Campaña : Form
+    {
+        public Campaña()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Está seguro que desea salir?", "Advertencia", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CarteleriaDigital/CarteleriaDigital/Pantallas/PantallaInicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarteleriaDigital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PanBannerSimple abrir = new PanBannerSimple();
+            abrir.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Está seguro que desea salir?", "Advertencia", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Campaña abrir = new Campaña();
+            abrir.Show();
+            this.SetVisibleCore(false);
+        }
+    }
+}
