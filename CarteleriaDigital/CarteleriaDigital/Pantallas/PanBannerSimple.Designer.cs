8cdaef4aa795b1b4855ffa9edbf7cba92ef2ffb0
Jennifer <Jennifer@DESKTOP-R39PCAT>
2017-01-25 10:33:01 -0300

    pantalla seleccionar banner y demás

--- CarteleriaDigital/CarteleriaDigital/Pantallas/PanBannerSimple.Designer.cs
+++ CarteleriaDigital/CarteleriaDigital/Pantallas/PanBannerSimple.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PanBannerSimple));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -39,9 +45,57 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Salir";
+            this.button1.Text = "Atrás";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Location = new System.Drawing.Point(40, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 47);
+            this.button2.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.button2, "Agregar Banner");
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Location = new System.Drawing.Point(175, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 47);
+            this.button3.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.button3, "Eliminar Banner");
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Location = new System.Drawing.Point(310, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 47);
+            this.button4.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.button4, "Modificar Banner");
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Location = new System.Drawing.Point(445, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 47);
+            this.button5.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.button5, "Vista Previa Banner");
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // PanBannerSimple
             // 
@@ -50,11 +104,15 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(634, 411);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PanBannerSimple";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PanBannerSimple";
+            this.Text = "BannerSimple";
             this.Load += new System.EventHandler(this.PanBannerSimple_Load);
             this.ResumeLayout(false);
 
@@ -63,5 +121,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
